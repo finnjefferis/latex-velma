cb83b865dca9c343dbe8bc74d8525543cc2a55bd
Finn Jefferis <91462667+finnjefferis@users.noreply.github.com>
2025-01-13 00:28:57 +0000

    updated scopes

--- app/api/spotify/auth/route.ts
+++ app/api/spotify/auth/route.ts
@@ -4,13 +4,11 @@ export async function GET() {
   const clientId = process.env.SPOTIFY_CLIENT_ID!;
   const redirectUri = process.env.SPOTIFY_REDIRECT_URI!;
   const scopes = [
-    "playlist-read-private",
-    "playlist-read-collaborative",
     "playlist-modify-public",
     "playlist-modify-private",
-    "user-read-private",
-    "user-read-email",
+    "playlist-read-private",
   ].join(" ");
+  
 
   // Generate Spotify authorization URL
   const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${encodeURIComponent(
